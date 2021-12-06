47d85dc1b8c62e0c79094e4e7e3a7b39a1622759
arogya <arogyabichpuria16@gmail.com>
2021-12-06 22:57:45 +0530

    updated the app.js and global css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import AppHeader from './components/core/AppHeader/AppHeader';
 import GlobalStyle from './global.css';
 import MainComponent from './components/core/MainComponent/MainComponent';
 import {BrowserRouter , Route , Routes} from 'react-router-dom';
+import Config from './pages/Config';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <AppHeader />
+      
     <Routes>
      <Route path='/'  element={<MainComponent />} exact />
+     <Route path='/config' element={<Config />} /> 
     </Routes>
     </BrowserRouter>
   );
