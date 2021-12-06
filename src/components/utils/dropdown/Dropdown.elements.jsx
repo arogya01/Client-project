18dd5cd10b811a610ff330e263ec9071edb31c45
arogya <arogyabichpuria16@gmail.com>
2021-12-06 22:46:29 +0530

    :wrench: updated the dropdown element

--- src/components/utils/dropdown/Dropdown.elements.jsx
+++ src/components/utils/dropdown/Dropdown.elements.jsx
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const DropdownContainer=styled.div`
  display: flex;
  flex-flow: column wrap;
- 
-`
+ align-items: center;
+ `
--- src/components/utils/dropdown/Dropdown.jsx
+++ src/components/utils/dropdown/Dropdown.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
-import { SelectBox,InputLabel } from '../../../global.elements';
-import {DropdownContainer} from './Dropdown.elements'
+import React from "react";
+import { InputLabel, SelectBox } from "../../../global.elements";
+import { DropdownContainer } from "./Dropdown.elements";
 
-const Dropdown = (options,value) => {
-    return (
+const Dropdown = ({
+  serviceName,
+  options,
+  value,
+  disabled,
+  setValue,
+  onChange,
+}) => {
+  return disabled ? (
     <DropdownContainer>
-        <InputLabel>Group Service</InputLabel>
-        <SelectBox option={options} value={value}>
-
-        </SelectBox>
+      <InputLabel>{serviceName}</InputLabel>
+      <SelectBox
+        width="218px"
+        disabled
+        onChange={(event) => {
+          onChange(event.target.value, setValue);
+        }}
+      >
+        {options.map((option) => {
+          return (
+            <option value={option} key={option}>
+              {option}
+            </option>
+          );
+        })}
+      </SelectBox>
     </DropdownContainer>
-        )
-}
+  ) : (
+    <DropdownContainer>
+      <InputLabel>{serviceName}</InputLabel>
+      <SelectBox
+        width="218px"
+        onChange={(event) => {
+          onChange(event.target.value, setValue);
+        }}
+      >
+        {options.map((option) => {
+          return (
+            <option value={option} key={option}>
+              {option}
+            </option>
+          );
+        })}
+      </SelectBox>
+    </DropdownContainer>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
