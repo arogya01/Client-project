67caa24f4e201b4c0a5dc251652d49624e2fdc98
arogya <arogyabichpuria16@gmail.com>
2021-12-06 22:55:36 +0530

    updated the utils components

--- src/components/utils/CTA/CTA.elements.jsx
+++ src/components/utils/CTA/CTA.elements.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const SectionWrapper=styled.div`
 width: 100%;
-padding: 1rem;
+padding:0.5rem;
 margin-top: 5rem;
 text-align: center;
 box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
@@ -18,9 +18,9 @@ margin: 0.5rem;
 `
 
 export const InputDetails=styled.input`
-width: 200px;
-height: 12px;
+width: 100%;    
+height: 36px;
 padding:0.5rem;
-border-radius: 10px;
+border-radius: 4px;
 
 `
--- src/components/utils/button/Btn.jsx
+++ src/components/utils/button/Btn.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {LinkButton,BtnLink} from './Btn.elements';
 
-export const Btn = ({btnName,LinkName}) => {
-    const path=`/${LinkName}`
+export const Btn = ({btnName,linkName}) => {
+    const path=`/${linkName}`
 
     return (
-         <LinkButton >{btnName}</LinkButton>
+        <BtnLink to={path}>
+        <LinkButton >
+        {btnName}
+        </LinkButton>
+        </BtnLink>
     )
 }
