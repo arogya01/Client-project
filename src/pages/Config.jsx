33d34818aeec77e147cb5356e0be7b2724e6742f
arogya <arogyabichpuria16@gmail.com>
2021-12-06 22:59:04 +0530

    updated the config page

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -1,27 +1,67 @@
-import React from 'react'
-import AppHeader from '../components/core/AppHeader/AppHeader'
-import { ConfigBtn } from '../components/utils/configButton/ConfigBtn'
-import Dropdown from '../components/utils/dropdown/Dropdown'
-import {FilterWrapper} from '../global.elements'
-import configData from '../data/config.json';
-import Table from '../components/core/Table/Table'
+import React from "react";
+import AppHeader from "../components/core/AppHeader/AppHeader";
+import { useState} from "react";
+import { ConfigBtn } from "../components/utils/configButton/ConfigBtn";
+import Dropdown from "../components/utils/dropdown/Dropdown";
+import { FilterWrapper } from "../global.elements";
+import configData from "../data/config.json";
+import Table from "../components/core/Table/Table";
 
-const Config = () => {
-    return (
-        <>
-        <AppHeader headerName="Configuration" /> 
-        <FilterWrapper>
-        {
-            configData[0].services.map(service => {
-                return <Dropdown serviceName={service.serviceName} key={service.serviceName} options={service.options} />
-            })
-        }
-        </FilterWrapper>
-        <ConfigBtn />
-        <Table />
-        {/* <DataTable /> */}
-        </>
-    )
+const onValueChange = (newValue, setValue) => {
+  setValue(newValue);
+
+};
+
+const checkUp= (value) =>{
+   if(value !== undefined){
+     return false;
+   }
+   return true;
 }
 
-export default Config
+const Config = () => {
+  const [valueOne, setValueOne] = useState(undefined);
+  const [valueTwo, setValueTwo] = useState(undefined);
+  const [valueThree, setValueThree] = useState(undefined);
+  
+
+  
+  return (
+    <>
+      <AppHeader headerName="Configuration" />
+      <FilterWrapper>
+        <Dropdown
+          serviceName={configData[0].serviceName}
+          key={configData[0].serviceName}
+          options={configData[0].options}
+          value={valueOne}
+          setValue={setValueOne}
+          onChange={onValueChange}
+        />
+        <Dropdown
+          serviceName={configData[1].serviceName}
+          key={configData[1].serviceName}
+          options={configData[1].options}
+          value={valueTwo}
+          disabled={checkUp(valueOne)}
+          setValue={setValueTwo}
+          onChange={onValueChange}
+        />
+        <Dropdown
+          serviceName={configData[2].serviceName}
+          key={configData[2].serviceName}
+          options={configData[2].options}
+          value={valueThree}
+          setValue={setValueThree}
+          onChange={onValueChange}
+          disabled={checkUp(valueTwo)}
+        />
+      </FilterWrapper>
+      <ConfigBtn />
+      {valueOne && valueTwo && valueThree && <Table />}
+      {/* <DataTable /> */}
+    </>
+  );
+};
+
+export default Config;
