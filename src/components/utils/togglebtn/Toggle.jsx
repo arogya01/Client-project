9d0c4ca29042fc6396b693191188940c7473c789
arogya <arogyabichpuria16@gmail.com>
2021-12-12 23:26:31 +0530

    updated the toggle button

--- src/components/utils/togglebtn/Toggle.jsx
+++ src/components/utils/togglebtn/Toggle.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
-export const Toggle = () => {
-    return (
-        <div className='toggle-wrapper'>
-        <input class='checkbox' type='checkbox' name='switch' id='switch' />
-        <label for='switch' className='toggle-label'></label>
-      </div>
-    )
-}
+export const Toggle = ({
+  isToggleValue,
+  setToggleValue
+}) => {
+  return (
+    <div className="toggle-wrapper">
+      <input
+        className="checkbox"
+        type="checkbox"
+        name="switch"
+        id="switch"
+        checked={isToggleValue}
+        onChange={() => setToggleValue(!isToggleValue)}
+      />
+      <label htmlFor="switch" className="toggle-label"></label>
+    </div>
+  );
+};
